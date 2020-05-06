d170a4a82a2f14ef7478fadfa695f4e6a4794419
ansuro <ansuro@users.noreply.github.com>
2020-05-06 18:10:27 +0200

    UI: SPS-Status-Enum to String
    ValueToString Hilfsfunktionen addiert. Das geht bestimmt noch anders und schöner.

---
+++ BohrUI/Helpers/Conversion/EnumTPAToString.js
@@ -0,0 +1,33 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="D:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="D:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="D:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="D:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="D:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var EnumTPAToString = function (par1) {
+        //console.log(par1);
+        switch (par1) {
+            case 0:
+                return 'startet';
+
+            case 1:
+                return 'gestartet';
+
+            case 2:
+                return 'stoppt';
+
+            case 3:
+                return 'gestoppt';
+
+            default:
+                return 'Unknown state: ' + par1;
+        }
+    };
+    
+    TcHmi.Functions.registerFunction('EnumTPAToString', EnumTPAToString);
+})(TcHmi);
---
+++ BohrUI/Helpers/Conversion/EnumTPBToString.js
@@ -0,0 +1,32 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="D:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="D:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="D:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="D:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="D:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var EnumTPBToString = function (par1) {
+        switch (par1) {
+            case 0:
+                return 'synchronsierend';
+
+            case 1:
+                return 'synchronisiert';
+
+            case 2:
+                return 'anhaltend';
+
+            case 3:
+                return 'angehalten';
+
+            default:
+                return 'Unknown state: ' + par1;
+        }
+    };
+    
+    TcHmi.Functions.registerFunction('EnumTPBToString', EnumTPBToString);
+})(TcHmi);
